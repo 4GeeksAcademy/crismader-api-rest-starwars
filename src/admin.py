121ca23d99c1f5921e985c37dae4b32d2a30f983
Cristian Trapiello <119610379+CrisMader@users.noreply.github.com>
2026-01-19 11:19:01 +0000

    Endpoints Favorites

--- src/admin.py
+++ src/admin.py
@@ -1,27 +1,20 @@
 import os
 from flask_admin import Admin
-from models import db, User, Character, Planet, Film, Vehicle, Specie, Favorite_character, Favorite_planet, Favorite_film, Favorite_vehicle, Favorite_specie
+from models import db, User, Character, Planet, Favorite_character, Favorite_planet
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin = Admin(app, name='Cris Admin', template_mode='bootstrap3')
 
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Character, db.session))
     admin.add_view(ModelView(Planet, db.session))
-    admin.add_view(ModelView(Film, db.session))
-    admin.add_view(ModelView(Vehicle, db.session))
-    admin.add_view(ModelView(Specie, db.session))
 
     admin.add_view(ModelView(Favorite_character, db.session))
     admin.add_view(ModelView(Favorite_planet, db.session))
-    admin.add_view(ModelView(Favorite_film, db.session))
-    admin.add_view(ModelView(Favorite_vehicle, db.session))
-    admin.add_view(ModelView(Favorite_specie, db.session))
-
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Character, Planet, Film, Vehicle, Specie
+from models import db, User, Character, Planet, Favorite_character, Favorite_planet
+
 # from models import Person
 
 app = Flask(__name__)
@@ -78,42 +79,6 @@ def get_planets():
 
     return jsonify(response_body), 200
 
-@app.route('/film', methods=['GET'])
-def get_films():
-    all_films = Film.query.all()
-    results_films = list(map(lambda film: film.serialize(), all_films))
-
-    response_body = {
-        "msg": "Hello, this is your GET /film response ",
-        "films": results_films
-    }
-
-    return jsonify(response_body), 200
-
-@app.route('/vehicle', methods=['GET'])
-def get_vehicles():
-    all_vehicles = Vehicle.query.all()
-    results_vehicles = list(map(lambda vehicle: vehicle.serialize(), all_vehicles))
-
-    response_body = {
-        "msg": "Hello, this is your GET /film response ",
-        "vehicles": results_vehicles
-    }
-
-    return jsonify(response_body), 200
-
-@app.route('/specie', methods=['GET'])
-def get_species():
-    all_species = Specie.query.all()
-    results_species = list(map(lambda specie: specie.serialize(), all_species))
-
-    response_body = {
-        "msg": "Hello, this is your GET /film response ",
-        "species": results_species
-    }
-
-    return jsonify(response_body), 200
-
 #Conseguir un elemento en especifico de la lista para poder modificarla o borrarla.
 
 @app.route('/user/<int:user_id>', methods=['GET'])
@@ -140,54 +105,6 @@ def get_character(character_id):
     return jsonify({
         "msg": "Hello, this is your GET /character/<id> response",
         "character": character.serialize()
-    }), 200
-
-@app.route('/planet/<int:planet_id>', methods=['GET'])
-def get_planet(planet_id):
-    planet = Planet.query.get(planet_id)
-
-    if planet is None:
-        return jsonify({"msg": "User not found"}), 404
-
-    return jsonify({
-        "msg": "Hello, this is your GET /planet/<id> response",
-        "planet": planet.serialize()
-    }), 200
-
-@app.route('/film/<int:film_id>', methods=['GET'])
-def get_film(film_id):
-    film = Film.query.get(film_id)
-
-    if film is None:
-        return jsonify({"msg": "User not found"}), 404
-
-    return jsonify({
-        "msg": "Hello, this is your GET /film/<id> response",
-        "film": film.serialize()
-    }), 200
-
-@app.route('/vehicle/<int:vehicle_id>', methods=['GET'])
-def get_vehicle(vehicle_id):
-    vehicle = Vehicle.query.get(vehicle_id)
-
-    if vehicle is None:
-        return jsonify({"msg": "Vehicle not found"}), 404
-
-    return jsonify({
-        "msg": "Hello, this is your GET /vehicle/<id> response",
-        "vehicle": vehicle.serialize()
-    }), 200
-
-@app.route('/specie/<int:specie_id>', methods=['GET'])
-def get_specie(specie_id):
-    specie = Specie.query.get(specie_id)
-
-    if specie is None:
-        return jsonify({"msg": "User not found"}), 404
-
-    return jsonify({
-        "msg": "Hello, this is your GET /specie/<id> response",
-        "specie": specie.serialize()
     }), 200
 
 #Hacer post de cada endpoint
@@ -275,90 +192,6 @@ def create_planet():
         "planet": new_planet.serialize()
     }), 201
 
-@app.route('/film', methods=['POST'])
-def create_film():
-    body = request.get_json()
-
-    if body is None:
-        return jsonify({"msg": "Missing JSON body"}), 400
-
-    name = body.get("name")
-    description = body.get("description")
-    imageLink = body.get("imageLink")
-
-    if not name or not description or not imageLink:
-        return jsonify({"msg": "name, description and imageLink are required"}), 400
-
-    new_film = Film(
-        name=name,
-        description=description,
-        imageLink=imageLink
-    )
-
-    db.session.add(new_film)
-    db.session.commit()
-
-    return jsonify({
-        "msg": "Film created",
-        "film": new_film.serialize()
-    }), 201
-
-@app.route('/vehicle', methods=['POST'])
-def create_vehicle():
-    body = request.get_json()
-
-    if body is None:
-        return jsonify({"msg": "Missing JSON body"}), 400
-
-    name = body.get("name")
-    description = body.get("description")
-    imageLink = body.get("imageLink")
-
-    if not name or not description or not imageLink:
-        return jsonify({"msg": "name, description and imageLink are required"}), 400
-
-    new_vehicle = Vehicle(
-        name=name,
-        description=description,
-        imageLink=imageLink
-    )
-
-    db.session.add(new_vehicle)
-    db.session.commit()
-
-    return jsonify({
-        "msg": "Vehicle created",
-        "vehicle": new_vehicle.serialize()
-    }), 201
-
-@app.route('/specie', methods=['POST'])
-def create_specie():
-    body = request.get_json()
-
-    if body is None:
-        return jsonify({"msg": "Missing JSON body"}), 400
-
-    name = body.get("name")
-    description = body.get("description")
-    imageLink = body.get("imageLink")
-
-    if not name or not description or not imageLink:
-        return jsonify({"msg": "name, description and imageLink are required"}), 400
-
-    new_specie = Specie(
-        name=name,
-        description=description,
-        imageLink=imageLink
-    )
-
-    db.session.add(new_specie)
-    db.session.commit()
-
-    return jsonify({
-        "msg": "Specie created",
-        "specie": new_specie.serialize()
-    }), 201
-
 #Hacer una actualización de datos de un elemento especifico de cualquier categoria
 
 @app.route('/character/<int:character_id>', methods=['PUT'])
@@ -405,73 +238,8 @@ def update_planet(planet_id):
         "planet": planet.serialize()
     }), 200
 
-@app.route('/film/<int:film_id>', methods=['PUT'])
-def update_film(film_id):
-    film = Film.query.get(film_id)
-
-    if film is None:
-        return jsonify({"msg": "Film not found"}), 404
-
-    body = request.get_json(silent=True)
-    if body is None:
-        return jsonify({"msg": "Missing JSON body"}), 400
-
-    film.name = body.get("name", film.name)
-    film.description = body.get("description", film.description)
-    film.imageLink = body.get("imageLink", film.imageLink)
-
-    db.session.commit()
-
-    return jsonify({
-        "msg": "Film updated",
-        "film": film.serialize()
-    }), 200
-
-@app.route('/vehicle/<int:vehicle_id>', methods=['PUT'])
-def update_vehicle(vehicle_id):
-    vehicle = Vehicle.query.get(vehicle_id)
-
-    if vehicle is None:
-        return jsonify({"msg": "Vehicle not found"}), 404
-
-    body = request.get_json(silent=True)
-    if body is None:
-        return jsonify({"msg": "Missing JSON body"}), 400
-
-    vehicle.name = body.get("name", vehicle.name)
-    vehicle.description = body.get("description", vehicle.description)
-    vehicle.imageLink = body.get("imageLink", vehicle.imageLink)
-
-    db.session.commit()
-
-    return jsonify({
-        "msg": "Vehicle updated",
-        "vehicle": vehicle.serialize()
-    }), 200
-
-@app.route('/specie/<int:specie_id>', methods=['PUT'])
-def update_specie(specie_id):
-    specie = Specie.query.get(specie_id)
-
-    if specie is None:
-        return jsonify({"msg": "Specie not found"}), 404
-
-    body = request.get_json(silent=True)
-    if body is None:
-        return jsonify({"msg": "Missing JSON body"}), 400
-
-    specie.name = body.get("name", specie.name)
-    specie.description = body.get("description", specie.description)
-    specie.imageLink = body.get("imageLink", specie.imageLink)
-
-    db.session.commit()
-
-    return jsonify({
-        "msg": "Specie updated",
-        "specie": specie.serialize()
-    }), 200
-
 #Borrar elementos especificos
+
 @app.route('/character/<int:character_id>', methods=['DELETE'])
 def delete_character(character_id):
     character = Character.query.get(character_id)
@@ -496,41 +264,101 @@ def delete_planet(planet_id):
 
     return jsonify({"msg": "Planet deleted"}), 200
 
-@app.route('/film/<int:film_id>', methods=['DELETE'])
-def delete_film(film_id):
-    film = Film.query.get(film_id)
+#Añadir favoritos a User
 
-    if film is None:
-        return jsonify({"msg": "Film not found"}), 404
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['POST'])
+def add_favorite_character(user_id, character_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
 
-    db.session.delete(film)
+    character = Character.query.get(character_id)
+    if character is None:
+        return jsonify({"msg": "Character not found"}), 404
+
+    existing_favorite = Favorite_character.query.filter_by(
+        user_id=user_id,
+        character_id=character_id
+    ).first()
+
+    if existing_favorite is not None:
+        return jsonify({"msg": "Character already in favorites"}), 409
+
+    favorite = Favorite_character(
+        user_id=user_id,
+        character_id=character_id
+    )
+
+    db.session.add(favorite)
     db.session.commit()
 
-    return jsonify({"msg": "Film deleted"}), 200
+    return jsonify({
+        "msg": "Favorite character added",
+        "favorite": favorite.serialize()
+    }), 201
 
-@app.route('/vehicle/<int:vehicle_id>', methods=['DELETE'])
-def delete_vehicle(vehicle_id):
-    vehicle = Vehicle.query.get(vehicle_id)
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['DELETE'])
+def delete_favorite_character(user_id, character_id):
+    favorite = Favorite_character.query.filter_by(
+        user_id=user_id,
+        character_id=character_id
+    ).first()
 
-    if vehicle is None:
-        return jsonify({"msg": "Vehicle not found"}), 404
+    if favorite is None:
+        return jsonify({"msg": "Favorite character not found"}), 404
 
-    db.session.delete(vehicle)
+    db.session.delete(favorite)
     db.session.commit()
 
-    return jsonify({"msg": "Vehicle deleted"}), 200
+    return jsonify({"msg": "Favorite character deleted"}), 200
 
-@app.route('/specie/<int:specie_id>', methods=['DELETE'])
-def delete_specie(specie_id):
-    specie = Specie.query.get(specie_id)
+#Añadir favoritos planetas
 
-    if specie is None:
-        return jsonify({"msg": "Specie not found"}), 404
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(user_id, planet_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
 
-    db.session.delete(specie)
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        return jsonify({"msg": "Planet not found"}), 404
+
+    existing_favorite = Favorite_planet.query.filter_by(
+        user_id=user_id,
+        planet_id=planet_id
+    ).first()
+
+    if existing_favorite is not None:
+        return jsonify({"msg": "Planet already in favorites"}), 409
+
+    favorite = Favorite_planet(
+        user_id=user_id,
+        planet_id=planet_id
+    )
+
+    db.session.add(favorite)
     db.session.commit()
 
-    return jsonify({"msg": "Specie deleted"}), 200
+    return jsonify({
+        "msg": "Favorite planet added",
+        "favorite": favorite.serialize()
+    }), 201
+
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(user_id, planet_id):
+    favorite = Favorite_planet.query.filter_by(
+        user_id=user_id,
+        planet_id=planet_id
+    ).first()
+
+    if favorite is None:
+        return jsonify({"msg": "Favorite planet not found"}), 404
+
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return jsonify({"msg": "Favorite planet deleted"}), 200
 
 
 # this only runs if `$ python src/app.py` is executed
